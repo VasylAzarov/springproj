b1a297fbdb4fb3de9296d37dfd3799dc621e387a
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-12-03 12:31:44 +0100

    try to fix exception 2

--- src/test/java/com.example.mate/MateApplicationTests.java
+++ src/test/java/com.example.mate/MateApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.mate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"spring.profiles.active=test"})
+@SpringBootTest
 public class MateApplicationTests {
 
     @Test
