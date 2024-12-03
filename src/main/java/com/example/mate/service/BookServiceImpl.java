e8de3dc89222dd88436d2020378ec0d096ea4b71
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-12-03 10:34:59 +0100

    cleanUp code

--- src/main/java/com/example/mate/service/BookServiceImpl.java
+++ src/main/java/com/example/mate/service/BookServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.mate.service;
 import com.example.mate.model.Book;
 import com.example.mate.repository.BookRepository;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +29,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book update(Book book) {
-        if(bookRepository.findById(book.getId()).isPresent()){
+        if (bookRepository.findById(book.getId()).isPresent()) {
             return bookRepository.save(book);
         }
         return null;
