b111fe008f7d6e10722f30630f2d995ed685bdc3
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-12-11 11:33:49 +0100

    minor fixes

--- src/main/java/dev/vasyl/proj/dto/user/UserLoginResponseDto.java
+++ src/main/java/dev/vasyl/proj/dto/user/UserLoginResponseDto.java
@@ -1,16 +1,13 @@
 package dev.vasyl.proj.dto.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
-public class UserLoginResponseDto {
-    @Schema(description = "User jtw token",
-            example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
-                    + "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4"
-                    + "gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJ"
-                    + "SMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
-    private String token;
+public record UserLoginResponseDto(
+        @Schema(description = "User jtw token",
+                example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                        + "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4"
+                        + "gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJ"
+                        + "SMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+        String token) {
+
 }
