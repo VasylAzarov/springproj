289020e53ddb221e186549bad771775ecadd90a9
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-12-12 16:54:03 +0100

    cleanup

--- src/main/java/dev/vasyl/proj/dto/shopping/cart/UpdateCartItemRequestDto.java
+++ src/main/java/dev/vasyl/proj/dto/shopping/cart/UpdateCartItemRequestDto.java
@@ -2,7 +2,6 @@ package dev.vasyl.proj.dto.shopping.cart;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 public record UpdateCartItemRequestDto(
         @Schema(description = "Operation increase(be default) quantity or decrease quantity",
