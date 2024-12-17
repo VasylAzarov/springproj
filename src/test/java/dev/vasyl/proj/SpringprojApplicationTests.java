3c8c22c9b367309fc0299218f9f16f78da64992b
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-12-17 14:51:36 +0100

    temporal removed Test contextLoads | add docker + env | update pom.xml

--- src/test/java/dev/vasyl/proj/SpringprojApplicationTests.java
+++ src/test/java/dev/vasyl/proj/SpringprojApplicationTests.java
@@ -1,12 +1,8 @@
 package dev.vasyl.proj;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SpringprojApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 }
